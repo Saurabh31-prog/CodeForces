cb9e4ddafde68f62c498568fac685a617f17803a
saurabh Mungle <munglesaurabh7284@gmail.com>
2024-09-21 23:21:22 +0530

    Add waytolongwords.java

---
+++ waytolongwords.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class waytolongwords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        for (int i = 0; i < n; i++) {
+            String word = sc.nextLine();
+            
+            if (word.length() > 10) {
+                String abbreviation = word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+                System.out.println(abbreviation);
+            } else {
+                System.out.println(word);
+            }
+        }
+        
+        sc.close();
+    }
+}
